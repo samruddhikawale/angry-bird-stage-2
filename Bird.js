d5b53fe5d1c8d4a965ce2cdddfb710dccf6d8405
samruddhikawale <79152055+samruddhikawale@users.noreply.github.com>
2021-03-18 19:06:29 +0530

    first upload

---
+++ Bird.js
@@ -0,0 +1,22 @@
+class Bird extends BaseClass {
+    constructor(x, y) {
+     super(x,y,50,50);
+    };
+    display(){
+      var pos = this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('blue')
+      fill('red')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
